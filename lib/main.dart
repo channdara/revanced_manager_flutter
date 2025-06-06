17deaefa173dd3c574d54ae296e3487809845370
e.chandara <eamchandara@labs.haystack.asia>
2025-06-06 17:11:00 +0700

    tldr: disable impeller renderer engine

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'src/base/base_stateful.dart';
-import 'src/common/app_text_style.dart';
-import 'src/common/common.dart';
+import 'src/common/app_theme.dart';
 import 'src/manager/callback_manager.dart';
 import 'src/manager/download_manager.dart';
 import 'src/manager/preferences_manager.dart';
@@ -58,80 +57,8 @@ class _ApplicationState extends BaseStateful<Application> {
     return MaterialApp(
       home: const SplashScreen(),
       themeMode: _themeMode ?? widget.themeMode,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        fontFamily: AppCommon.fontFamily,
-        colorSchemeSeed: _accentColor ?? widget.accentColor,
-        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            elevation: 0.0,
-            shadowColor: Colors.transparent,
-          ),
-        ),
-        iconTheme: const IconThemeData(color: Colors.black),
-        cardTheme: const CardThemeData(
-          margin: EdgeInsets.zero,
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-        ),
-        appBarTheme: const AppBarTheme(
-          titleTextStyle: AppTextStyle.appBarTitleTextStyle,
-          actionsPadding: EdgeInsets.only(right: 8.0),
-        ),
-        progressIndicatorTheme: const ProgressIndicatorThemeData(
-          year2023: false,
-        ),
-        navigationBarTheme: NavigationBarThemeData(
-          labelTextStyle: MaterialStateProperty.resolveWith((states) {
-            return states.contains(WidgetState.selected)
-                ? AppTextStyle.s12Bold
-                : AppTextStyle.s12;
-          }),
-        ),
-        tabBarTheme: TabBarThemeData(
-          labelStyle:
-              AppTextStyle.s14.copyWith(fontFamily: AppCommon.fontFamily),
-          unselectedLabelStyle:
-              AppTextStyle.s14.copyWith(fontFamily: AppCommon.fontFamily),
-        ),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: AppCommon.fontFamily,
-        colorSchemeSeed: _accentColor ?? widget.accentColor,
-        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            elevation: 0.0,
-            shadowColor: Colors.transparent,
-          ),
-        ),
-        iconTheme: const IconThemeData(color: Colors.white),
-        cardTheme: const CardThemeData(
-          margin: EdgeInsets.zero,
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-        ),
-        appBarTheme: const AppBarTheme(
-          titleTextStyle: AppTextStyle.appBarDarkTitleTextStyle,
-          actionsPadding: EdgeInsets.only(right: 8.0),
-        ),
-        progressIndicatorTheme: const ProgressIndicatorThemeData(
-          year2023: false,
-        ),
-        navigationBarTheme: NavigationBarThemeData(
-          labelTextStyle: MaterialStateProperty.resolveWith((states) {
-            return states.contains(WidgetState.selected)
-                ? AppTextStyle.s12Bold
-                : AppTextStyle.s12;
-          }),
-        ),
-        tabBarTheme: TabBarThemeData(
-          labelStyle:
-              AppTextStyle.s14.copyWith(fontFamily: AppCommon.fontFamily),
-          unselectedLabelStyle:
-              AppTextStyle.s14.copyWith(fontFamily: AppCommon.fontFamily),
-        ),
-      ),
+      theme: AppTheme.lightTheme(_accentColor ?? widget.accentColor),
+      darkTheme: AppTheme.darkTheme(_accentColor ?? widget.accentColor),
     );
   }
 }
---
+++ lib/src/common/app_theme.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'app_text_style.dart';
+import 'common.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme(Color colorSchemeSeed) {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      fontFamily: AppCommon.fontFamily,
+      colorSchemeSeed: colorSchemeSeed,
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      elevatedButtonTheme: _elevatedButtonTheme,
+      iconTheme: const IconThemeData(color: Colors.black),
+      cardTheme: _cardTheme,
+      appBarTheme: const AppBarTheme(
+        scrolledUnderElevation: 0.0,
+        backgroundColor: Colors.transparent,
+        titleTextStyle: AppTextStyle.appBarTitleTextStyle,
+        actionsPadding: EdgeInsets.only(right: 8.0),
+      ),
+      progressIndicatorTheme: _progressIndicatorTheme,
+      navigationBarTheme: _navigationBarTheme,
+      tabBarTheme: _tabBarTheme,
+    );
+  }
+
+  static ThemeData darkTheme(Color colorSchemeSeed) {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      fontFamily: AppCommon.fontFamily,
+      colorSchemeSeed: colorSchemeSeed,
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      elevatedButtonTheme: _elevatedButtonTheme,
+      iconTheme: const IconThemeData(color: Colors.white),
+      cardTheme: _cardTheme,
+      appBarTheme: const AppBarTheme(
+        scrolledUnderElevation: 0.0,
+        backgroundColor: Colors.transparent,
+        titleTextStyle: AppTextStyle.appBarDarkTitleTextStyle,
+        actionsPadding: EdgeInsets.only(right: 8.0),
+      ),
+      progressIndicatorTheme: _progressIndicatorTheme,
+      navigationBarTheme: _navigationBarTheme,
+      tabBarTheme: _tabBarTheme,
+    );
+  }
+
+  static final ElevatedButtonThemeData _elevatedButtonTheme =
+      ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0.0,
+      shadowColor: Colors.transparent,
+    ),
+  );
+
+  static const CardThemeData _cardTheme = CardThemeData(
+    margin: EdgeInsets.zero,
+    clipBehavior: Clip.antiAliasWithSaveLayer,
+  );
+
+  static const ProgressIndicatorThemeData _progressIndicatorTheme =
+      ProgressIndicatorThemeData(year2023: false);
+
+  static final NavigationBarThemeData _navigationBarTheme =
+      NavigationBarThemeData(
+    labelTextStyle: MaterialStateProperty.resolveWith((states) {
+      return states.contains(WidgetState.selected)
+          ? AppTextStyle.s12Bold
+          : AppTextStyle.s12;
+    }),
+  );
+
+  static final TabBarThemeData _tabBarTheme = TabBarThemeData(
+    labelStyle: AppTextStyle.s14.copyWith(fontFamily: AppCommon.fontFamily),
+    unselectedLabelStyle:
+        AppTextStyle.s14.copyWith(fontFamily: AppCommon.fontFamily),
+  );
+}
